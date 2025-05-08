c138fcebb6bb0b89ba05ef23f9bee862f888d1a9
Bayel Dzhoroev <badzoroev@team.amocrm.com>
2025-05-08 23:46:53 +0300

    fix: fix magic frog layout

--- src/components/LandingPage/TopSection/Background/Background.tsx
+++ src/components/LandingPage/TopSection/Background/Background.tsx
@@ -4,7 +4,7 @@ import { IconSlider } from '../IconSlider/IconSlider';
 export const Background = () => {
   return (
     <div id="top-section-bg" className="h-full absolute w-full">
-      <div className="z-1 absolute opacity-20 right-[10rem] top-[0rem] h-[70rem] text-deep-purple md:right-0 md:left-0 md:top-20 md:block md:h-150 lg:h-220 lg:top-20">
+      <div className="z-1 absolute opacity-20 right-[10rem] top-0 h-[70rem] text-deep-purple md:right-0 md:left-0 md:top-20 md:h-[30rem] lg:h-[50rem] lg:left-auto lg:right-[45rem] lg:top-[-4rem]">
         <MagicFrog />
       </div>
       <div className="absolute bottom-0 w-full">
